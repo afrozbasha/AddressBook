8b44d013fd92ae427dad355f3aceeac67c4ededa
afrozbasha <iafrozbasha@gmail.com>
2021-07-19 19:00:54 +0530

    Ability to create a Contacts in Address Book

--- AddressBook.java
+++ AddressBook.java
@@ -1,8 +1,21 @@
 package com.AddressBook;
+import java.util.Scanner;
 
-public class Book {
 
+public class AddressBook {
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book");
+        AddinBook b = new AddinBook();
+        System.out.println("Welcome to Address Book Program");
+
+        b.setFirstName("Afroz");
+        b.setLastName("Basha");
+        b.setCellNum("95xx605");
+        b.setEmail("afrozxx@gmail.com");
+        b.setCity("jammalamadugu");
+        b.setZip("516434");
+        b.setState("Andhra Pradesh");
+
+        System.out.println(" First Name: "+b.getFirstName()+"\n Last Name: "+b.getLastName()+"\n Cell Num: "+b.getCellNum()+"\n Email-id: "+b.getEmail()+"\n City: "+b.getCity()+" \n Pin: "+b.getZip()+"\n State: "+b.getState());
+
     }
 }
